83226ad565ed662490f2988bec29b86230df4834
Bryant <bryantbrewster@gmail.com>
2022-08-07 14:19:36 -0500

    test

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+# test to add new stuff
+
 
 app = Flask(__name__)
 
