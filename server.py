aab028646d2676d164cdee5659935c1dd075c8ac
Bryant <bryantbrewster@gmail.com>
2022-08-07 14:01:37 -0500

    original upload

---
+++ server.py
@@ -0,0 +1,276 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+# create database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workout_log.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# initializes database
+db = SQLAlchemy(app)
+
+
+# creating the tables
+class Exercises(db.Model):
+    workout_id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Integer, nullable=False)
+    workout = db.Column(db.String(250))
+    exercise = db.Column(db.String(250))
+    sets =  db.Column(db.Integer)
+    reps = db.Column(db.Integer)
+    weight = db.Column(db.Float)
+
+class Routines(db.Model):
+    workout = db.Column(db.String(250), primary_key=True, nullable=False)
+    routine_name = db.Column(db.String(250))
+
+
+db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/choose-a-workout')
+def workout_choice():
+    full_workout_log = db.session.query(Exercises.workout.distinct()).all()
+    workout_day_names = []
+    for i in full_workout_log:
+        workout_day_names.append(i[0])
+    return render_template("workout.html", workout_list=workout_day_names)
+
+
+@app.route('/enter-your-stats', methods=['GET', 'POST'])
+def add_to_workout():
+    if request.form.get("button", False) == "Record a Workout":
+        workout_list_box_result = request.form.get('Workout_ListBox')
+        print(workout_list_box_result)
+        workout_date = request.form.get('workout_date')
+        exercise_list = []
+        exercise_tuples = db.session.query(Exercises.exercise.distinct()).filter(Exercises.workout == workout_list_box_result).all()
+        for i in exercise_tuples:
+            exercise_list.append(i[0])
+        print(exercise_list)
+        number_of_exercises = len(exercise_list)
+        # feed the list of exercises into the entry.html, and render them on the new page (form)
+        return render_template('entry.html', exercises_in_selected_workout=exercise_list,
+                               workout_name=workout_list_box_result, workout_date=workout_date,
+                               number_of_exercises=number_of_exercises)
+    if request.form.get("button", False) == "Edit Routine":
+        workout_list_box_result = request.form.get('Workout_ListBox')
+        db.session.query(Routines).delete()
+        db.session.commit()
+        exercise_list = []
+        exercise_tuples = db.session.query(Exercises.exercise.distinct()).filter(Exercises.workout == workout_list_box_result)
+        for i in exercise_tuples:
+            exercise_list.append(i[0])
+        for exercise in exercise_list:
+            new_routine = Routines(
+                workout=exercise,
+                routine_name=workout_list_box_result
+            )
+            db.session.add(new_routine)
+            db.session.commit()
+        print(exercise_list)
+        print(f"edit button responded correctly to try and edit {workout_list_box_result}")
+        return render_template('edit_routine.html', exercise_list=exercise_list, routine_name=workout_list_box_result)
+    if request.form.get("button", False) == "Delete Routine":
+        workout_list_box_result = request.form.get('Workout_ListBox')
+        for row in db.session.query(Exercises):
+            if row.workout == workout_list_box_result:
+                db.session.delete(row)
+        db.session.commit()
+        # full_workout_log = db.session.query(Exercises.workout.distinct()).all()
+        # workout_day_names = []
+        # for i in full_workout_log:
+        #     workout_day_names.append(i[0])
+        # return render_template("workout.html", workout_list=workout_day_names)
+        return redirect(url_for('workout_choice'))
+    else:
+        # full_workout_log = db.session.query(Exercises.workout.distinct()).all()
+        # workout_day_names = []
+        # for i in full_workout_log:
+        #     workout_day_names.append(i[0])
+        # return render_template("workout.html", workout_list=workout_day_names)
+        return redirect(url_for('workout_choice'))
+
+
+
+
+@app.route("/", methods=['GET', 'POST'])
+def completed_exercises():
+    # look into how to either stay on the same page (w/ the other fields STILL filled out), or use 1 submit button to
+    # submit all the exercise forms at once. Because right now if someone fills out/submits one, it clears the others
+    if request.method == "POST":
+        exercises_in_workout_length = int(request.form["Number_of_Exercises"])
+        for i in range(1, int(exercises_in_workout_length) + 1):
+            if request.form[f"Weight{i}"] == '':
+                weight_provided = None
+                new_workout = Exercises(
+                    date=request.form[f"Date{i}"],
+                    workout=request.form[f"Workout{i}"],
+                    exercise=request.form[f"Exercise{i}"],
+                    sets=request.form[f"Sets{i}"],
+                    reps=request.form[f"Reps{i}"],
+                    weight=weight_provided
+                )
+                db.session.add(new_workout)
+                db.session.commit()
+            else:
+                new_workout = Exercises(
+                    date=request.form[f"Date{i}"],
+                    workout=request.form[f"Workout{i}"],
+                    exercise=request.form[f"Exercise{i}"],
+                    sets=request.form[f"Sets{i}"],
+                    reps=request.form[f"Reps{i}"],
+                    weight=request.form[f"Weight{i}"]
+                )
+                db.session.add(new_workout)
+                db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        pass
+
+
+# @app.route("/test/<string:exercise_list>", methods=['GET', 'POST'])
+# def test_func(exercise_list):
+#     print(exercise_list)
+#     return redirect(url_for('home'))
+
+
+# @app.route("/new-routine", methods=['GET', 'POST'])
+# def new_routine():
+#     if request.method == "POST":
+#         exercise_list = request.form.get('Exercise_List')
+#         # exercise_list = request.form["Exercise_List"]
+#         print(type(exercise_list))
+#         exercise_to_add = request.form["Exercise_Name"]
+#         print(exercise_list)
+#         # print(exercise_to_add)
+#         # exercise_list.append(exercise_to_add)
+#         return render_template('add_workout.html', exercise_list=exercise_list)
+#     else:
+#         exercise_list = []
+#         print(type(exercise_list))
+#         return render_template('add_workout.html', exercise_list=exercise_list)
+
+@app.route("/new-routine", methods=['GET', 'POST'])
+def new_routine():
+    if request.method == "POST":
+        new_routine = Routines(
+            workout=request.form["Exercise_Name"]
+        )
+        db.session.add(new_routine)
+        db.session.commit()
+        exercise_list = []
+        exercise_tuples = db.session.query(Routines.workout.distinct()).all()
+        for i in exercise_tuples:
+            exercise_list.append(i[0])
+        return render_template('add_workout.html', exercise_list=exercise_list)
+    else:
+        db.session.query(Routines).delete()
+        db.session.commit()
+        return render_template('add_workout.html')
+
+
+@app.route("/new_routine", methods=['GET', 'POST'])
+def submit_new_routine():
+    new_routine_name = request.form["routine_name"]
+    for row in db.session.query(Routines):
+        row.routine_name = new_routine_name
+        new_workout = Exercises(
+            workout=row.routine_name,
+            exercise=row.workout
+        )
+        db.session.add(new_workout)
+    db.session.commit()
+    return redirect(url_for('workout_choice'))
+
+
+@app.route("/delete")
+def delete_during_creation():
+    # right now the delete button works, but it redirects the user to another page
+    # change it so that you can delete one, and it will reload the page and keep what the
+    # user already had entered (maybe this can be done by -> when delete() is invoked,
+    # it saves the other names and feeds them back into the add_workout.html to
+    # "start" with those names already in there
+    exercise_name = request.args.get('exercise_name')
+    print(exercise_name)
+    exercise_to_delete = Routines.query.get(exercise_name)
+    db.session.delete(exercise_to_delete)
+    db.session.commit()
+    exercise_list = []
+    exercise_tuples = db.session.query(Routines.workout.distinct()).all()
+    for i in exercise_tuples:
+        exercise_list.append(i[0])
+    return render_template('add_workout.html', exercise_list=exercise_list)
+
+
+# @app.route("/edit-routine")
+# def edit_routine():
+#     db.session.query(Routines).delete()
+#     db.session.commit()
+#     routine_to_edit = request.args.get('routine_to_edit')
+#     exercise_list = []
+#     exercise_tuples = db.session.query(Exercises.exercise.distinct()).filter(Exercises.workout == routine_to_edit).all()
+#     for i in exercise_tuples:
+#         exercise_list.append(i[0])
+#     for exercise in exercise_list:
+#         new_routine = Routines(
+#             workout=exercise
+#         )
+#         db.session.add(new_routine)
+#         db.session.commit()
+#     return render_template('add_workout.html', exercise_list=exercise_list)
+
+@app.route("/edit-routine")
+def delete_from_routine():
+    exercise_name = request.args.get('exercise_name')
+    routine_name = request.args.get('routine_name')
+    print(routine_name)
+    exercise_to_delete = Routines.query.get(exercise_name)
+    db.session.delete(exercise_to_delete)
+    db.session.commit()
+    exercise_list = []
+    exercise_tuples = db.session.query(Routines.workout.distinct()).all()
+    for i in exercise_tuples:
+        exercise_list.append(i[0])
+    return render_template('edit_routine.html', exercise_list=exercise_list, routine_name=routine_name)
+
+
+@app.route("/save", methods=['GET', 'POST'])
+def save_routine_edits():
+    # get routine_name
+    # get the remaining exercise names and put them in a "keep" list
+    # iterate through the database and find the rows where the routine name in the db == this routine name
+    # "change" the routine_name, even if it's the same
+    # check to see if the exercise name in the db is included in the "keep" list, and if not, delete it from the db
+    new_routine_name = request.form['new_routine_name']
+    old_routine_name = request.form['old_routine_name']
+    exercise_keep_list = []
+    exercise_tuples = db.session.query(Routines.workout.distinct()).all()
+    for i in exercise_tuples:
+        exercise_keep_list.append(i[0])
+    print(f'new_routine_name is {new_routine_name}')
+    print(f'old_routine_name is {old_routine_name}')
+    print(exercise_keep_list)
+    for row in db.session.query(Exercises):
+        if row.workout == old_routine_name:
+            row.workout = new_routine_name
+            if row.exercise not in exercise_keep_list:
+                db.session.delete(row)
+            else:
+                pass
+        db.session.commit()
+            # MAKE SURE THAT IT WORKS WITH A NEW NAME TOO, ALSO DELETING ZERO EXERCISES AND IT STILL WORKING, etc.
+    return render_template('index.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
