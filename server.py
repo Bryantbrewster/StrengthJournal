0218a56190d3bcab7ec6df83fa5ba559f4e136c7
Bryant <bryantbrewster@gmail.com>
2023-04-26 01:55:13 -0500

    Added reps to personal best table

--- server.py
+++ server.py
@@ -272,9 +272,9 @@ def routine_dashboard():
     # WHERE e2.weight IS NULL AND e1.weight IS NOT NULL AND e1.DATE::varchar != 0::varchar AND e1.USER_ID={current_user.id} AND e1.workout='{routine}'
     # GROUP BY e1.exercise;'''
 
-    personal_records_sql = f'''SELECT DISTINCT ON(exercise) exercise_id, exercise, weight, date from exercises where USER_ID={current_user.id}
+    personal_records_sql = f'''SELECT DISTINCT ON(exercise) exercise_id, exercise, weight, reps, date from exercises where USER_ID={current_user.id}
     AND date IS NOT NULL AND weight IS NOT NULL AND workout='{routine}'
-    ORDER BY exercise, weight DESC, date, exercise_id'''
+    ORDER BY exercise, weight DESC, reps DESC, date, exercise_id'''
     # personal_records_sql = f'''SELECT * FROM exercises WHERE USER_ID={current_user.id} AND DATE != 0 AND
     # workout = '{routine}';'''
     personal_records_results = engine.execute(personal_records_sql).all()
